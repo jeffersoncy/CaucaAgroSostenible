b80e7e408ec257790d3787f27921be0e3bd94129
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2021-08-27 23:59:39 -0500

    microservicio gestion usuario terminado

--- Servicio-Usuario/src/main/java/co/unicauca/Usuario/ServicioUsuarioApplication.java
+++ Servicio-Usuario/src/main/java/co/unicauca/Usuario/ServicioUsuarioApplication.java
@@ -3,6 +3,11 @@ package co.unicauca.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Jefferson Campo y Danny Diaz
+ *
+ */
 @SpringBootApplication
 public class ServicioUsuarioApplication {
 
