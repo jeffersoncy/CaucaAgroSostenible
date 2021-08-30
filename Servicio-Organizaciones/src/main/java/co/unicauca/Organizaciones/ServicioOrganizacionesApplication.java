bd24f3f3b4074385a652956a10e2d28a8ae84157
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2021-08-30 10:12:11 -0500

    Creación de microservicio organizaciones

---
+++ Servicio-Organizaciones/src/main/java/co/unicauca/Organizaciones/ServicioOrganizacionesApplication.java
@@ -0,0 +1,19 @@
+package co.unicauca.Organizaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Método de ejecución
+ * 
+ * @author Jefferson Campo y Danny Diaz
+ *
+ */
+@SpringBootApplication
+public class ServicioOrganizacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioOrganizacionesApplication.class, args);
+	}
+
+}
---
+++ Servicio-Organizaciones/src/test/java/co/unicauca/Organizaciones/ServicioOrganizacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.Organizaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioOrganizacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
