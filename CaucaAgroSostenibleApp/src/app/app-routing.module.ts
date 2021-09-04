f3aaea63aff3c60f2618bec68f7c2ceacc6f37e1
damd2212 <dannyad@unicauca.edu.co>
2021-09-03 21:44:54 -0500

    listar en prueba

--- CaucaAgroSostenibleApp/src/app/app-routing.module.ts
+++ CaucaAgroSostenibleApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarComponent } from './Producto/agregar/agregar.component';
+import { EditarComponent } from './Producto/editar/editar.component';
+import { EliminarComponent } from './Producto/eliminar/eliminar.component';
+import { ListarComponent } from './Producto/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"listar", component:ListarComponent},
+  {path:"agregar", component:AgregarComponent},
+  {path:"editar", component:EditarComponent},
+  {path:"eliminar", component:EliminarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
