c1a8d2a46c4d025bdf930dde2d73bdfcf7d295af
Christian-Tobar <70769281+Christian-Tobar@users.noreply.github.com>
2021-09-09 15:28:49 -0500

    config

---
+++ Servicio-Producto/bin/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO productos(ID, NOMPRODUCTO, RUTAIMAGEN, CANTIDAD, PRECIO) values(1, 'Arroz', 'local1', 100 , 2000);
+INSERT INTO productos(ID, NOMPRODUCTO, RUTAIMAGEN, CANTIDAD, PRECIO) values(2, 'Papa', 'local2', 150 , 3000);
+INSERT INTO productos(ID, NOMPRODUCTO, RUTAIMAGEN, CANTIDAD, PRECIO) values(3, 'Aceite', 'local3', 200 , 5000);
+INSERT INTO productos(ID, NOMPRODUCTO, RUTAIMAGEN, CANTIDAD, PRECIO) values(4, 'Cebolla', 'local4', 250 , 4000);
