5fe858e4816050bed2d7cc4e0617f3fbbabf0a10
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2021-09-22 00:48:43 -0500

    ofertas

--- Servicio-Ofertas/src/main/resources/import.sql
+++ Servicio-Ofertas/src/main/resources/import.sql
@@ -1,10 +1,8 @@
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1001,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1002,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1003,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1004,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1005,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1006,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1007,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1008,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1009,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
-INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1010,'Bulto papa',100000,'offcarnes.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1001,'Limon Tahiti 2 kg',6300,'limontahiti.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1002,'Mango Tommi maduro 50 kg',25000,'mango.jpeg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1003,'Maracuya 2 kg',100000,'maracuya.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1004,'Papa Criolla 50 kg',100000,'papacriolla.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1005,'Papa Parda 50 kg',100000,'papaparda.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1006,'Papaya 3 kg',100000,'papaya.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1007,'Tomate Arbol 3 kg',100000,'tomatearbol.jpg', 2, 0.5);
+INSERT INTO oferta(id, nomoferta, precio, rutaimg, cantidad, descuento) values(1008,'Remolacha 50 kg',100000,'remolacha.jpg', 2, 0.5);
