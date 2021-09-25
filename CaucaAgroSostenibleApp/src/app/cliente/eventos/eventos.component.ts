9362864cd871191167f6d8c383712cbdd4da3ed9
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2021-09-25 01:24:48 -0500

    eventos

--- CaucaAgroSostenibleApp/src/app/cliente/eventos/eventos.component.ts
+++ CaucaAgroSostenibleApp/src/app/cliente/eventos/eventos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Evento } from 'src/app/Modelo/Evento';
+import { ServiceService } from 'src/app/Service/service.service';
 
 @Component({
   selector: 'app-eventos',
@@ -9,9 +12,21 @@ export class EventosComponent implements OnInit {
   title = 'Eventos del Departamento del Cauca';
   lat = 2.4453523;
   lng = -76.6136989;
-  constructor() { }
+
+  UbicacionCentral: Evento;
+
+  coordenadas: Evento  [] = [];
+  constructor(private service:ServiceService, private router:Router) { }
 
   ngOnInit(): void {
+    this.UbicacionCentral = new Evento(this.lat,this.lng);
+  }
+
+  obtenerDatos(){
+    this.service.listarEventos()
+    .subscribe(data=>{
+      this.coordenadas=data;
+    })
   }
 
 }
