073d6e2d2ed25357df5c6b003ced3ceea5c2d6e2
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2021-09-25 16:34:07 -0500

    agregar evento

--- CaucaAgroSostenibleApp/src/app/administrador/Eventos/agregar-evento-admin/agregar-evento-admin.component.ts
+++ CaucaAgroSostenibleApp/src/app/administrador/Eventos/agregar-evento-admin/agregar-evento-admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 import { Evento } from 'src/app/Modelo/Evento';
+import { ServiceService } from 'src/app/Service/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-evento-admin',
@@ -14,16 +16,26 @@ export class AgregarEventoAdminComponent implements OnInit {
 
   UbicacionCentral: Evento;
 
-  coordenadas: Evento  [] = [];
-  constructor() { }
+  eventos:Evento[];
+  evento:Evento = new Evento(0,0,"","");
+  
+  constructor(private service:ServiceService, private router:Router) { }
 
   ngOnInit(): void {
+    this.obtenerDatos();
     this.UbicacionCentral = new Evento(this.lat,this.lng,"","");
   }
 
   mapClicked($event: MouseEvent){
     let coord = new Evento($event.coords.lat, $event.coords.lng,"","");
-    this.coordenadas.push(coord);
+    this.eventos.push(coord);
+  }
+
+  obtenerDatos(){
+    this.service.listarEventos()
+    .subscribe(data=>{
+      this.eventos=data;
+    })
   }
 
 }
