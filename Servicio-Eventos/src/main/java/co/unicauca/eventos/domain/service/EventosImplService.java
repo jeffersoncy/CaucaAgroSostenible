09578d8bac0a2a93a7147cf1346ca925858830d6
damd2212 <dannyad@unicauca.edu.co>
2021-09-25 00:18:01 -0500

    Listo el micro servicio

--- Servicio-Eventos/src/main/java/co/unicauca/eventos/domain/service/EventosImplService.java
+++ Servicio-Eventos/src/main/java/co/unicauca/eventos/domain/service/EventosImplService.java
@@ -1,12 +1,16 @@
 package co.unicauca.eventos.domain.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import co.unicauca.eventos.presentation.exceptions.EventosError;
 import co.unicauca.eventos.access.dao.IEventosDao;
 import co.unicauca.eventos.domain.entity.Eventos;
+import co.unicauca.eventos.presentation.exceptions.EventosDomainException;
 import co.unicauca.eventos.presentation.exceptions.ResourceNotFoundException;
 
 @Service
@@ -36,6 +40,34 @@ public class EventosImplService implements IEventosService{
 			throw new ResourceNotFoundException();
 		}
 		return event;
+	}
+
+	@Override
+	@Transactional
+	public Eventos create(Eventos evento) throws EventosDomainException{
+		List <EventosError> errors = validateDomain(evento);
+		
+		if(!errors.isEmpty()) {
+			throw new EventosDomainException(errors);
+		}
+		
+		return eventosDao.save(evento);
+	}
+	
+	
+	private List<EventosError> validateDomain(Eventos event){
+		List<EventosError> errors = new ArrayList<>();
+		
+		if(event.getNombre() == null || event.getNombre().isBlank()) {
+			errors.add(new EventosError(EnumErrorCodes.EMPTY_FIELD, "nombre", "El nombre no puede ser nulo"));
+		}
+		
+		if(event.getDescripcion() == null || event.getDescripcion().isBlank()) {
+			errors.add(new EventosError(EnumErrorCodes.EMPTY_FIELD, "descripcion", "La descripcion no puede ser nula"));
+		}
+		
+		
+		return errors;
 	}
 	
 }
--- Servicio-Eventos/src/main/java/co/unicauca/eventos/domain/service/IEventosService.java
+++ Servicio-Eventos/src/main/java/co/unicauca/eventos/domain/service/IEventosService.java
@@ -3,13 +3,18 @@ package co.unicauca.eventos.domain.service;
 import java.util.List;
 
 import co.unicauca.eventos.presentation.exceptions.ResourceNotFoundException;
+import co.unicauca.eventos.presentation.exceptions.EventosDomainException;
 import co.unicauca.eventos.domain.entity.Eventos;
 
 public interface IEventosService {
+	
+	public Eventos create(Eventos evento) throws EventosDomainException;
 	
 	public List<Eventos> findAll();
 	
 	public Eventos findById(Long id)throws ResourceNotFoundException;
 	
 	public void deleteByID(Long id) throws ResourceNotFoundException;
+	
+	
 }
