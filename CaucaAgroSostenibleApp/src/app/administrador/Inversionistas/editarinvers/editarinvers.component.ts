7b3d4afe62dad25964900170b21441b7a5c811e8
Christian-Tobar <70769281+Christian-Tobar@users.noreply.github.com>
2021-09-28 08:10:28 -0500

    actualizar imagen

--- CaucaAgroSostenibleApp/src/app/administrador/Inversionistas/editarinvers/editarinvers.component.ts
+++ CaucaAgroSostenibleApp/src/app/administrador/Inversionistas/editarinvers/editarinvers.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Error } from 'src/app/Modelo/Error';
 import { Inversionista } from 'src/app/Modelo/Inversionista';
 import { ServiceService } from 'src/app/Service/service.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-editarinvers',
@@ -11,10 +12,13 @@ import { ServiceService } from 'src/app/Service/service.service';
 })
 export class EditarInversComponent implements OnInit {
 
+  public previsualizacion: String;
+  public ruta: string;
+  public archivo: any = []
   invers: Inversionista = new Inversionista;
   errores: Error[];
   bandera: boolean = false;
-  constructor(private service: ServiceService, private router: Router) { }
+  constructor(private service: ServiceService, private router: Router, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.obtenerDatos();
@@ -24,8 +28,42 @@ export class EditarInversComponent implements OnInit {
     let id = localStorage.getItem("idInvers");
     this.service.getInversionistaByID(+id).subscribe(data => {
       this.invers = data;
+      console.log(this.invers.rutaimg);
+      this.previsualizacion = "../../../../assets/Inversionistas/" + data.rutaimg;
     })
   }
+
+  capturarFile(event):any{
+    alert("Imagen agregada correctamente");
+    const archivoCapturado = event.target.files[0]
+    this.extraerBase64(archivoCapturado).then((imagen:any) => {
+      this.previsualizacion = imagen.base;
+    })
+    this.archivo.push(archivoCapturado);
+    this.invers.rutaimg = this.invers.rutaimg.slice(12);
+    
+  }
+
+  extraerBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve ({
+          base:reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      };
+    } catch (e) {
+      return null;
+    }
+  })
 
   actualizarInvers(invers: Inversionista) {
     this.service.editInversionista(invers).subscribe(data => {
